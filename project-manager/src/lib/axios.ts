27e7ae81d70a8a0be2625fcffe7db0d08e7c3f6c
Adil Aulia Azuri <adil.azuri145@gmail.com>
2025-10-05 13:40:19 +0700

    [FIX] FE : change env variable call

--- project-manager/src/lib/axios.ts
+++ project-manager/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseURL = process.env.API_URL;
+export const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 const api = axios.create({
     baseURL: baseURL,
